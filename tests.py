ebfe6978f05e3c95574fd4c365f007523fe84fa8
AbdullahNoori <nooriabdullah86@gmail.com>
2019-10-11 15:14:43 -0700

    added needed libraries for testing

---
+++ tests.py
@@ -0,0 +1,7 @@
+from unittest import TestCase, main as unittest_main, mock
+from flask import Flask
+from app import app
+from bson.objectid import ObjectId
+
+if __name__ == '__main__':
+    unittest_main()
